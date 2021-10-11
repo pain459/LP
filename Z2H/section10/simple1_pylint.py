c7c996b0498652b92aa3734a6aabb6cbcb017ab3
pain459 <ravi.ameer@gmail.com>
2021-10-11 17:49:58 +0530

    Massive commit with changes

---
+++ Z2H/section10/simple1_pylint.py
@@ -0,0 +1,16 @@
+'''
+A Very simple script
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1  # First variable
+    second = 2  # Second variable
+
+    print(first)  # Printing variable1
+    print(second)  # Printing variable2
+
+
+myfunc()
