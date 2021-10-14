ae2a2e5d6e5d8ef640c0e1b525284637b69a48af
pain459 <ravi.ameer@gmail.com>
2021-10-14 18:47:23 +0530

    exercises & scripts

--- PMC/1-SITWOP/helloWorld.py
+++ PMC/1-SITWOP/helloWorld.py
@@ -6,3 +6,17 @@ print("""Triple quote to use any symbols like ' * " """)
 print(1 + 2)
 print(2 * 10)
 print(3 - 1)
+
+print('sentence1', 'sentence2', 'sentence3', 6)
+
+a = 10
+b = 11
+print(f'{a} and {b}')
+
+a = '123'
+print(a * 2)
+
+a = '1'
+b = print(a + '1')
+
+type(b)
