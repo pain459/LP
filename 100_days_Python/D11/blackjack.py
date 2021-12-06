02921e6cd95f657eebaab0fa9f8da763610806d1
pain459 <ravi.ameer@gmail.com>
2021-12-06 20:41:40 +0530

    finished blackjack initial version.

--- 100_days_Python/D11/blackjack.py
+++ 100_days_Python/D11/blackjack.py
@@ -1,147 +1,112 @@
 import random
-from blackjack_logo import logo
+import blackjack_logo
 from random import choice
 
-cards_in_deck = [13, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
 def initialization():
-    print(logo)
+    print(blackjack_logo.logo)
 
 
-def start_cards():
+def give_2_random_cards_each(deck_of_cards):
+    """We will generate 2 random cards from the deck."""
     user_first_cards = [random.choice(
-        cards_in_deck), random.choice(cards_in_deck)]
+        deck_of_cards), random.choice(deck_of_cards)]
     computer_first_cards = [random.choice(
-        cards_in_deck), random.choice(cards_in_deck)]
+        deck_of_cards), random.choice(deck_of_cards)]
     print(f'Your cards [{user_first_cards[0]}, {user_first_cards[1]}]')
-    # Not displaying the computer second card. Instead we will display it as X.
+    # ------ #
+    # user_first_cards = [10, 11]  # test case.
+    # computer_first_cards = [13, 13]  # test case.
+    # ------ #
+    # Not displaying the computer second card. Instead, we will display it as X.
     print(f"Computer's first cards [{computer_first_cards[0]}, X]")
     return user_first_cards, computer_first_cards
 
 
-def check_win_loose(user_first_cards, computer_first_cards, user_cards, computer_cards):
-    if user_first_cards == [13, 13]:
-        print('User won!')
-        game_end(user_first_cards, computer_first_cards,
-                 user_cards, computer_cards)
-        # return
-    elif computer_first_cards == [13, 13]:
-        print('Computer won!')
-        game_end(user_first_cards, computer_first_cards,
-                 user_cards, computer_cards)
-        # return
-    elif sum(user_first_cards) == sum(computer_first_cards):
-        print('Game Draw. Dealer won!')
-        game_end(user_first_cards, computer_first_cards,
-                 user_cards, computer_cards)
-        # return
-    elif sum(user_first_cards) == 21:
-        print('User won blackjack! Count 21')
-        game_end(user_first_cards, computer_first_cards,
-                 user_cards, computer_cards)
-        # return
-    elif sum(computer_first_cards) == 21:
-        print('Computer won blackjack! Count 21')
-        game_end(user_first_cards, computer_first_cards,
-                 user_cards, computer_cards)
-        # return
-    elif sum(user_first_cards) > 21:
-        print('Computer Won as user cards are above count 21.')
-        game_end(user_first_cards, computer_first_cards,
-                 user_cards, computer_cards)
-        # return
-    elif sum(computer_first_cards) > 21:
-        print('User Won as computer cards are above count 21.')
-        game_end(user_first_cards, computer_first_cards,
-                 user_cards, computer_cards)
-        # return
+def validate_blackjack(user_cards, computer_cards):
+    print('Checking if user or computer have blackjack')
+    if sum(computer_cards) == 21:
+        print('Computer has blackjack')
+        return
     elif sum(user_cards) == 21:
-        print('User won! Found backjack with count 21.')
-        game_end(user_first_cards, computer_first_cards,
-                 user_cards, computer_cards)
-    elif sum(computer_cards) == 21:
-        print('Computer won! Found blackjack with count 21.')
-        game_end(user_first_cards, computer_first_cards,
-                 user_cards, computer_cards)
-    elif sum(user_cards) > 21:
-        print('Computer won. Card count exceeded 21 for user.')
-        game_end(user_first_cards, computer_first_cards,
-                 user_cards, computer_cards)
+        print('User has the blackjack')
+        return
+    else:
+        print('No blackjack detected!')
+        return user_draw_card(user_cards=user_cards, computer_cards=computer_cards)
 
 
-def validate_cards():
-    pass
-
-
-def draw_cards_user(user_cards, computer_cards):
-    print(user_cards)
-    end_turn_user = False
-    while not end_turn_user:
-        check_win_loose(user_first_cards=user_first_cards,
-                        computer_first_cards=computer_first_cards, user_cards=user_cards, computer_cards=computer_cards)
-        draw_card = str(
-            input("Type 'y' to get another card, type 'n' to pass: ")).lower()
-        if draw_card == 'y':
-            user_next_cards = int(random.choice(cards_in_deck))
-            user_cards.append(user_next_cards)
-            print(user_cards)
-            end_turn_user = False
-        elif draw_card == 'n':
-            print('Holding your position!')
-            print(user_cards)
-            end_turn_user = True
+def user_draw_card(user_cards, computer_cards, ):
+    if sum(user_cards) > 21:
+        # user_cards = [1 if i == 11 else i for i in user_cards]
+        if 11 in user_cards:
+            user_cards = [1 if i == 11 else i for i in user_cards]
+            if sum(user_cards) > 21:
+                print('User lost the game')
+                return
+            else:
+                return draw_another_card(user_cards=user_cards, deck_of_cards=cards, computer_cards=computer_cards)
         else:
-            print('Invalid option. Try again!')
-            end_turn_user = False
-
-
-def draw_cards_computer(computer_cards):
-    print(computer_cards)
-    chance = [1, 0]
-    end_turn_computer = False
-    while not end_turn_computer:
-        draw_card = random.choice(chance)
-        if draw_card == 1:
-            computer_next_cards = int(random.choice(cards_in_deck))
-            computer_cards.append(computer_next_cards)
-            print(computer_cards)
-            end_turn_computer = False
-        elif draw_card == 0:
-            print('Holding your position!')
-            print(computer_cards)
-            end_turn_computer = True
-        else:  # This is technically not required.
-            print('Invalid option. Try again!')
-            end_turn_computer = False
-
-
-def display_result():
-    pass
-
-
-def game_end(user_first_cards, computer_first_cards, user_cards, computer_cards):
-    user_first_cards = []
-    computer_first_cards = []
-    user_cards = []
-    computer_cards = []
-    stop_game = False
-    while not stop_game:
-        user_choice = str(input('Do you want to play again? y/n: '))
-        if user_choice == 'y':
-            print('Starting new game!')
-            stop_game = False
+            print(f'count is {sum(user_cards)}')
+            print('User lost.')
             return
-        elif user_choice == 'n':
-            print('Program will terminate now!')
-            stop_game = True
-        else:
-            print('Invalid option. Try again!')
-            stop_game = False
+    else:
+        print('No Ace in the cards!')
+        return draw_another_card(user_cards=user_cards, deck_of_cards=cards, computer_cards=computer_cards)
 
 
-initialization()
-user_first_cards, computer_first_cards = start_cards()
-draw_cards_user(user_cards=user_first_cards,
-                computer_cards=computer_first_cards)
-draw_cards_computer(computer_cards=computer_first_cards)
+def draw_another_card(user_cards, computer_cards, deck_of_cards=cards):
+    print('Another card needed!')
+    user_choice = str(input("Enter 'y' if you want a card. Enter 'n' to skip: "))
+    if user_choice == 'y':
+        user_cards.append(random.choice(cards))
+        print(user_cards)
+        return validate_blackjack(user_cards=user_cards, computer_cards=computer_cards)
+    elif user_choice == 'n':
+        print(f"User stands at {sum(user_cards)}")
+        return computer_draw_another_card(user_cards, computer_cards, deck_of_cards=cards)
+
+
+def computer_draw_another_card(user_cards, computer_cards, deck_of_cards=cards):
+    if sum(user_cards) < 17:
+        print('Computer won as the user count is less than 17.')
+    elif sum(user_cards) > 17:
+        computer_cards.append(random.choice(cards))
+        computer_turn_over = False
+        while not computer_turn_over:
+            if 21 > sum(user_cards) > 17 and sum(computer_cards) <= 21:
+                if sum(computer_cards) > sum(user_cards):
+                    return computer_won_game(computer_cards=computer_cards, user_cards=user_cards)
+                # computer_turn_over = True
+                elif sum(computer_cards) == sum(user_cards):
+                    return draw_game(user_cards=user_cards, computer_cards=computer_cards)
+                # computer_turn_over = True
+                elif sum(computer_cards) < sum(user_cards):
+                    return user_won_game(computer_cards=computer_cards, user_cards=user_cards)
+            computer_turn_over = True
+
+
+def computer_won_game(computer_cards, user_cards):
+    print(f"Computer won! Score is {sum(computer_cards)} vs {sum(user_cards)}")
+
+
+def user_won_game(user_cards, computer_cards):
+    print(f"User won! Score is {sum(user_cards)} vs {sum(computer_cards)}")
+
+
+def draw_game(user_cards, computer_cards):
+    print(f"Its a Draw! Score is {sum(user_cards)} vs {sum(computer_cards)}")
+
+
+def main():
+    initialization()
+    cards_user, cards_computer = give_2_random_cards_each(deck_of_cards=cards)
+    # print(cards_user) # Spoilers
+    # print(cards_computer) # spoilers
+    validate_blackjack(user_cards=cards_user, computer_cards=cards_computer)
+
+
+if __name__ == "__main__":
+    main()
