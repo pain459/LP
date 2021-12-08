9efba8c5f24211be87346bcbbe969e34a0d4d2e0
Ravi <ravi.ameer@gmail.com>
2021-12-08 22:36:58 +0530

    Added some basics of OOP

---
+++ 100_days_Python/D16/turtle_demo.py
@@ -0,0 +1,12 @@
+import turtle
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape('turtle')
+timmy.color('red')
+turtle.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
