beaa86de97eb9fa5c4c08fb74adc72f9d94eb1e1
Ravi <ravi.ameer@gmail.com>
2023-11-25 07:41:09 +0530

    age calculator - made the app more functional in nature.

--- clever_prog/ppfb/5_age_calculator.py
+++ clever_prog/ppfb/5_age_calculator.py
@@ -3,15 +3,27 @@
 import datetime
 
 print("Welcome to the age calculator")
-_year = int(input("Enter the year of birth: "))
-_month = int(input("Enter the month of birth: "))
-_day = int(input("Enter the day of birth: "))
-_dob = datetime.date(_year, _month, _day)
 
-print("Enter DOB is", _dob)
 
-_today = datetime.date.today()
+def _prompt_age():
+    _year = int(input("Enter the year of birth: "))
+    _month = int(input("Enter the month of birth: "))
+    _day = int(input("Enter the day of birth: "))
+    _dob = datetime.date(_year, _month, _day)
+    # print("Entered DOB is", _dob)
+    return _dob
 
-_age = int((_today - _dob).days / 365.25) # Logic to calculate age in days.
 
-print("Calculated age is", _age)
+def _today():
+    _current_day = datetime.date.today()
+    return _current_day
+
+
+def _calculate_age(_dob, _today):
+    _age = int((_today - _dob).days / 365.25)  # Logic to calculate age in days.
+    return _age
+
+
+if __name__ == "__main__":
+    # _prompt_age()
+    print("Calculated age is", _calculate_age(_prompt_age(), _today()))
