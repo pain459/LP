c5c0c35738ca3e9a3ff5ad40b4b6b34289e7a229
Ravi <ravi.ameer@gmail.com>
2024-01-01 23:41:57 +0530

    calculate execution time in python.

---
+++ clever_prog/ppfb/9_calculate_execution_time.py
@@ -0,0 +1,22 @@
+from time import time
+
+start = time()
+
+# Python program to create acronyms
+word = "Artificial Intelligence"
+text = word.split()
+a = ""
+for i in text:
+    a = a + str(i[0]).upper()
+print(a)
+
+# one more dummy piece of code to calculate the execution time.
+x = 0
+for j in range(10000000):
+    x += j
+print(x)
+
+end = time()
+
+execution_time = end - start
+print("Execution time is: ", execution_time)
