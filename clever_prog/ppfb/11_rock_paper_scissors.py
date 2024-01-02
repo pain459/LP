e13c63d57485f5b5c0d4703ed31e01b4be13ee3e
Ravi <ravi.ameer@gmail.com>
2024-01-02 07:59:57 +0530

    game winning rules defined.

--- clever_prog/ppfb/11_rock_paper_scissors.py
+++ clever_prog/ppfb/11_rock_paper_scissors.py
@@ -10,3 +10,15 @@ if player1 not in standard_choices:
 player2 = random.choice(standard_choices).lower()
 print(player1)
 print("Player2 selected:", player2)
+
+# Game winning and loosing conditions.
+if player1 == "rock" and player2 == "paper":
+    print("Player 2 wins!")
+elif player1 == "paper" and player2 == "scissor":
+    print("Player 2 wins!")
+elif player1 == "scissors" and player2 == "rock":
+    print("Player 2 Wins!")
+elif player1 == player2:
+    print("Tie!")
+else:
+    print("Player 1 Wins!")
