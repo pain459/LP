24729793bd0387387c7b0da3073c32c26542a529
Ravi <ravi.ameer@gmail.com>
2024-01-02 07:43:34 +0530

    Wrote a lambda function which I have no idea how it worked at this point.

--- clever_prog/ppfb/10_count_number_of_words_in_a_line.py
+++ clever_prog/ppfb/10_count_number_of_words_in_a_line.py
@@ -1,4 +1,5 @@
 import pandas as pd
 data = pd.read_csv("https://raw.githubusercontent.com/amankharwal/Website-data/master/articles.csv", encoding='latin1')
 print(data.head())
-
+data["Number or Words"] = data["Article"].apply(lambda n: len(n.split()))
+print(data.head())
