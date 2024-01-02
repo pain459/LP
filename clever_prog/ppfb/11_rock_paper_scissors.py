bdd162f02e5d4e2faa9a6bd8c1481a8561e3c71d
Ravi <ravi.ameer@gmail.com>
2024-01-02 07:53:44 +0530

    Added exit code in case of wrong choice.

--- clever_prog/ppfb/11_rock_paper_scissors.py
+++ clever_prog/ppfb/11_rock_paper_scissors.py
@@ -1,6 +1,11 @@
 import random
 
+standard_choices = ["rock", "paper", "scissors"]
 player1 = input("Select rock, paper or scissor: ").lower()
+if player1 not in standard_choices:
+    print("Not a valid choice. Exiting.")
+    exit(1)
+
 player2 = random.choice(["Rock", "Paper", "Scissor"]).lower()
 print(player1)
-print(player2)
+print("Player2 selected:", player2)
