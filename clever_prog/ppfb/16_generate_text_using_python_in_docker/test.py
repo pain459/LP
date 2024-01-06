9976d5bb360d0a7ced2d01bb5b8dadf3ca7f6564
Ravi <ravi.ameer@gmail.com>
2024-01-06 08:00:24 +0530

    Added basic program to perform sentimental analysis.

--- clever_prog/ppfb/16_generate_text_using_python_in_docker/test.py
+++ clever_prog/ppfb/16_generate_text_using_python_in_docker/test.py
@@ -1,1 +1,7 @@
-print("This is a test message from test.py")
+from transformers import pipeline
+
+print("Sentimental analysis program.")
+classifier = pipeline("Sentimental analysis")
+classifier("The existence of micro organisms proves that universe is infinite. Its just we are unable to see it yet.")
+
+
