fb85371c49718283ac33a6aed7bd72404539d164
Ravi <ravi.ameer@gmail.com>
2024-01-07 10:05:57 +0530

    some more modifications.

--- clever_prog/ppfb/qr_decoder_service/main.py
+++ clever_prog/ppfb/qr_decoder_service/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from pyzbar.pyzbar import decode
 from PIL import Image
 from io import BytesIO
@@ -9,11 +9,11 @@ app = FastAPI()
 async def upload_file(file: UploadFile = File(...)):
     contents = await file.read()
     decoded_data = decode_qr_code(contents)
-    return {"decoded_data": decoded_data}
+    if not decoded_data:
+        raise HTTPException(status_code=404, detail="No QR code found")
+    print("Decoded Data:", decoded_data[0].data.decode('utf-8'))
+    return {"decoded_data": decoded_data[0].data.decode('utf-8')}
 
 def decode_qr_code(image_bytes):
     img = Image.open(BytesIO(image_bytes))
-    decoded = decode(img)
-    if decoded:
-        return decoded[0].data.decode('utf-8')
-    return "No QR code found"
+    return decode(img)
