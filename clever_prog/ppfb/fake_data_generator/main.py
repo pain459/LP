516a65d039dc23cfeee534d4f16d8406170488a0
Ravi <ravi.ameer@gmail.com>
2024-01-07 11:54:44 +0530

    slight changes to main.py

--- clever_prog/ppfb/fake_data_generator/main.py
+++ clever_prog/ppfb/fake_data_generator/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from faker import Faker
 import pandas as pd
 
@@ -6,7 +6,7 @@ app = FastAPI()
 fake = Faker()
 
 @app.get("/generate_profiles/")
-def generate_profiles(num_records: int = 10):
+def generate_profiles(num_records: int = Query(default=10, ge=1, le=100)):
     data = [fake.profile() for _ in range(num_records)]
     df = pd.DataFrame(data)
     return df.to_dict(orient="records")
