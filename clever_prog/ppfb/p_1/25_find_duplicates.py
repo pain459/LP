8352e6809ab33bbdb6d04beb33672d7c0111c25f
Ravi <ravi.ameer@gmail.com>
2024-01-08 20:51:47 +0530

    Find duplicates program added.

--- clever_prog/ppfb/p_1/25_find_duplicates.py
+++ clever_prog/ppfb/p_1/25_find_duplicates.py
@@ -1,7 +1,14 @@
 # program to find the duplicates in python.
 
 def find_duplicates(x):
-    return 0
+    length = len(x)
+    duplicates = []
+    for i in range(length):
+        n = i + 1
+        for a in range(n, length):
+            if x[i] == x[a] and x[i] not in duplicates:
+                duplicates.append(x[i])
+    return duplicates
 
 
 user_input = ["one", "three", "four", "one", "five", "three"]
