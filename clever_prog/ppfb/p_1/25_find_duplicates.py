2b5e465d9ecca7b979ca65c2b95df342146498c3
Ravi <ravi.ameer@gmail.com>
2024-01-08 20:47:02 +0530

    Find duplicates skeleton added.

---
+++ clever_prog/ppfb/p_1/25_find_duplicates.py
@@ -0,0 +1,9 @@
+# program to find the duplicates in python.
+
+def find_duplicates(x):
+    return 0
+
+
+user_input = ["one", "three", "four", "one", "five", "three"]
+
+print(find_duplicates(user_input))
