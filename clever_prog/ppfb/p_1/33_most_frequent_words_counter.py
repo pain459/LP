d890b8ca13847f1b7eed74de2740e02972e5e72a
Ravi <ravi.ameer@gmail.com>
2024-01-13 17:40:30 +0530

    Added one line to observe to list.

--- clever_prog/ppfb/p_1/33_most_frequent_words_counter.py
+++ clever_prog/ppfb/p_1/33_most_frequent_words_counter.py
@@ -4,6 +4,8 @@ with open('economics.txt', 'r') as f:
     for line in f:
         words.extend(line.split())
 
+print(words) # This will split the line into words and form a list out of it.
+
 from collections import Counter
 counts = Counter(words)
 top5 = counts.most_common(5)
