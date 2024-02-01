79eb3402da994ed87a45e038b9ebbd9d2c03f5f7
Ravi <ravi.ameer@gmail.com>
2024-02-01 07:49:34 +0530

    Python program with doc strings.

---
+++ CPP/C3/docstrings_ex.py
@@ -0,0 +1,18 @@
+# program with 2 functions and docstrings
+
+def add(x, y):
+    """
+    This function takes 2 numbers and finds their sum.
+    It displays the sum as result.
+    """
+    print("Sum =", x + y)
+
+
+def message():
+    """
+    This function displays welcome message!
+    """
+    print("Welcome!")
+
+add(10, 6)
+message()
