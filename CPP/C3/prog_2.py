121f4fd8639070d3bebfd7d9bd5cf54ec5e37e8d
Ravi <ravi.ameer@gmail.com>
2024-02-02 08:18:12 +0530

    Converting octal, hexa and binary to integers.

---
+++ CPP/C3/prog_2.py
@@ -0,0 +1,12 @@
+# program to convert octal binary and hexadecimal into decimal number system.
+
+n1 = 0o17
+n2 = 0B1110010
+n3 = 0X1C2
+
+n = int(n1)
+print('Octal 17 =', n)
+n = int(n2)
+print('Binary 1110010 =', n)
+n = int(n3)
+print('Hexadecimal 1c2 =', n)
