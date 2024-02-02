07704d85baf4cd387b793695cb60a1eb79aa2f3f
Ravi <ravi.ameer@gmail.com>
2024-02-02 08:33:19 +0530

    Conversion advanced.

---
+++ CPP/C3/prog_3.py
@@ -0,0 +1,12 @@
+# Converting number from different system to decimal. v2.0
+
+n1 = "0o17"
+n2 = "0B1110010"
+n3 = "0X1C2"
+
+n = int(n1, 8)
+print(n)
+n = int(n2, 2)
+print(n)
+n = int(n3, 16)
+print(n)
