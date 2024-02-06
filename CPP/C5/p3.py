3d7d4726abe2597798c58c13aefcb699a62d8a71
Ravi <ravi.ameer@gmail.com>
2024-02-06 08:20:45 +0530

    p3 accepting character only from any given string.

---
+++ CPP/C5/p3.py
@@ -0,0 +1,5 @@
+# accepting a single character from the keyboard.
+
+ch1 = input("Enter a character: ")
+print("I will only consider the first character either ways.")
+print("The character is: ", ch1[0])
