2984411edffcf99a4be94d2d3637ce9a9607724c
Ravi <ravi.ameer@gmail.com>
2024-02-07 08:07:39 +0530

    Adding 2 numbers in 3 different ways.

---
+++ CPP/C5/p8.py
@@ -0,0 +1,8 @@
+# accept two inputs and add them
+
+x = int(input("Enter int number1: "))
+y = int(input("Enter int number2: "))
+
+print("The sum of", x, "and", y, "is", x + y)
+print("The sum of {} and {} is {}".format(x, y, x+y))
+print(f"The sum of {x} and {y} is {x + y}")
