85972673fb16d63125d6c0bfce1efb0d57f232c2
Ravi <ravi.ameer@gmail.com>
2024-02-14 07:14:37 +0530

    number convertor from other systems to binary.

---
+++ CPP/C5/p10.py
@@ -0,0 +1,13 @@
+# Convert numbers from other systems to decimal numbers
+
+str1 = input("Enter hexadecimal number: ")
+n = int(str1, 16)
+print("Hexadecimal to Decimal =", n)
+
+str2 = input("Enter a octal number: ")
+n = int(str2, 8)
+print("Octal to Decimal =", n)
+
+str3 = input("Enter a binary number: ")
+n = int(str3, 2)
+print("Binary to Decimal =", n)
