0e1f846060cca9b35c5b693af3972e24409ec05d
Ravi <ravi.ameer@gmail.com>
2024-02-16 08:29:13 +0530

    Find the area of the circle.

---
+++ CPP/C6/p1.py
@@ -0,0 +1,8 @@
+# to find area of a circle
+import math
+
+r = float(input("Enter the radius: "))
+area = math.pi * r ** 2
+print("Area of circle =", area)
+# flooring.
+print("Area of circle = {:0.2f}".format(area))
