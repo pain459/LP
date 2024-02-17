fde01463945875f84c230fcd471c124715cb3945
Ravi <ravi.ameer@gmail.com>
2024-02-17 17:58:53 +0530

    Find the number is greater, less than or equal to zero.

---
+++ CPP/C6/p7.py
@@ -0,0 +1,10 @@
+# to know the number is zero or positive or negative
+
+num = -5
+
+if num == 0:
+    print(num, "is zero.")
+elif num > 0:
+    print(num, "is greater than zero.")
+else:
+    print(num, "is less than zero.")
