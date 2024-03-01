695f01dd88fb97b529022daf9d65154456e0a217
Ravi <ravi.ameer@gmail.com>
2024-03-01 17:18:30 +0530

    array operations demo

---
+++ CPP/C7/p9.py
@@ -0,0 +1,12 @@
+# Operations on arrays
+
+from array import *
+from math import log
+
+# Create an array with int values
+# arr = array('f', [round(log(i, 2) ** 2, 2) for i in range(1, 10)])
+# arr = array('f', [float("{:.2f}".format(log(i, 2) ** 2)) for i in range(1, 10)])
+arr = array('f', [float(format(log(i, 2) ** 2, '.2f')) for i in range(1, 10)])
+
+
+print(arr)
