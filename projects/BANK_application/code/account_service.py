95d2b3acf58b322aead5a9234fff0f57efd8eda8
Ravi <ravi.ameer@gmail.com>
2024-03-03 20:01:56 +0530

    Duplicate account creation protection added.

--- projects/BANK_application/code/account_service.py
+++ projects/BANK_application/code/account_service.py
@@ -1,3 +1,4 @@
+# account_service.py
 from database_service import DatabaseService
 
 class AccountService:
@@ -5,10 +6,18 @@ class AccountService:
         self.db_service = DatabaseService(db_name)
 
     def create_account(self, user_id, account_type, initial_balance):
+        # Check if the user already has an account of the same type
+        user_accounts = self.db_service.get_accounts(user_id)
+        for account in user_accounts:
+            if account["account_type"] == account_type:
+                raise ValueError("Account of type '{}' already exists for user {}".format(account_type, user_id))
+
+        # If no duplicate account found, create a new account
         self.db_service.add_account(user_id, account_type, initial_balance)
 
     def get_user_accounts(self, user_id):
         return self.db_service.get_accounts(user_id)
+
 
 # Usage example:
 # account_service = AccountService("bank.db")
