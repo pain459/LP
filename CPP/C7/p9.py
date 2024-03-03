71333da393f5d5dd2986f7bf9c66729bd6afa3b4
Ravi <ravi.ameer@gmail.com>
2024-03-03 17:01:05 +0530

    Few array operations performed.

--- CPP/C7/p9.py
+++ CPP/C7/p9.py
@@ -7,6 +7,12 @@ from math import log
 # arr = array('f', [round(log(i, 2) ** 2, 2) for i in range(1, 10)])
 # arr = array('f', [float("{:.2f}".format(log(i, 2) ** 2)) for i in range(1, 10)])
 arr = array('f', [float(format(log(i, 2) ** 2, '.2f')) for i in range(1, 10)])
-
-
 print(arr)
+arr.append(30)
+arr.insert(0, 143)
+print(arr)
+n = arr.index(9)
+print(n)
+# Convert array to list
+list1 = arr.tolist()
+print(list1)
