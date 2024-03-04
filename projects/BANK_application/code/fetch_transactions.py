99175808491a620319f0798595b3fb4d6ba9797e
Ravi <ravi.ameer@gmail.com>
2024-03-04 22:14:09 +0530

    Need to fix the issue with fetch_transactions.py

--- projects/BANK_application/code/fetch_transactions.py
+++ projects/BANK_application/code/fetch_transactions.py
@@ -4,19 +4,24 @@ from account_service import AccountService
 # Create an instance of AccountService
 account_service = AccountService("bank.db")
 
+def fetch_transaction_history(account_id):
+    # Fetch the transaction history for the specified account_id
+    transactions = account_service.db_service.get_account_transactions(account_id)
+
+    # Print the transaction history
+    if transactions:
+        print("Transaction History for Account ID", account_id)
+        for transaction in transactions:
+            print("Transaction ID:", transaction['id'])
+            print("Amount:", transaction['amount'])
+            print("Transaction Type:", transaction['transaction_type'])
+            print("Timestamp:", transaction['timestamp'])
+            print()
+    else:
+        print("No transactions found for Account ID", account_id)
+
 # Specify the account_id for which you want to see the transaction history
-account_id = 1  # Replace 123 with the actual account ID
+account_id = 2  # Replace 123 with the actual account ID
 
-# Fetch the transaction history for the specified account_id
-transactions = account_service.db_service.get_account_transactions(account_id)
-
-# Print the transaction history
-if transactions:
-    print("Transaction History for Account ID", account_id)
-    for transaction in transactions:
-        print("Transaction ID:", transaction['id'])
-        print("Amount:", transaction['amount'])
-        print("Timestamp:", transaction['timestamp'])
-        print()
-else:
-    print("No transactions found for Account ID", account_id)
+# Call the function to fetch and print the transaction history
+fetch_transaction_history(account_id)
