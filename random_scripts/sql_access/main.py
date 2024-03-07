1b9428b4a6b1272f6cf0b49a7dbe5ad535714151
Ravi <ravi.ameer@gmail.com>
2024-03-07 23:49:36 +0530

    Execute the query with argument parser. Sample attatched.

--- random_scripts/sql_access/main.py
+++ random_scripts/sql_access/main.py
@@ -1,4 +1,9 @@
 import pyodbc
+import argparse
+
+# Constants
+# server = 'WATER'
+# database = 'AdventureWorks2022'
 
 
 def query_sql_server(server, database, query):
@@ -27,14 +32,30 @@ def query_sql_server(server, database, query):
         conn.close()
 
 
-# Example usage
-server = 'WATER'
-database = 'AdventureWorks2022'
-query = """
-        select ProductID,Name, ProductNumber, DaysToManufacture from Production.Product 
-        where DaysToManufacture > 1
-        and DaysToManufacture < 4
-        """
+def main():
+    # create argument parser
+    parser = argparse.ArgumentParser(description='Query SQL server.')
 
-result = query_sql_server(server=server, database=database, query=query)
-print(result)
+    # Add arguments
+    parser.add_argument('--server', required=True, help='SQL Server hostname or IP address')
+    parser.add_argument('--database', required=True, help='Database name')
+    parser.add_argument('--query_part', required=True, help='Part of your query')
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Construct full query
+    query = f"select ProductID,Name, ProductNumber, DaysToManufacture from Production.Product where {args.query_part}"
+
+    # Execute query
+    # result = query_sql_server(server=server, database=database, query=query)
+    result = query_sql_server(args.server, args.database, query)
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
+
+# Sample invocation
+# $ python main.py --server 'WATER' --database 'AdventureWorks2022' --query_part 'DaysToManufacture > 1 and DaysToManufacture < 4'
