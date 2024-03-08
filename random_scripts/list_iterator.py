5f77674d26e335628376ab6f3e0b55dd9c99a62c
Ravi <ravi.ameer@gmail.com>
2024-03-08 20:41:07 +0530

    Added logging.

--- random_scripts/list_iterator.py
+++ random_scripts/list_iterator.py
@@ -1,5 +1,19 @@
 import subprocess
+import logging
+from logging.handlers import RotatingFileHandler
 
+
+# Configure logging
+LOG_FILENAME = 'app.log'
+logging.basicConfig(level=logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler = RotatingFileHandler(LOG_FILENAME, maxBytes=5 * 1024 * 1024, backupCount=3)
+handler.setFormatter(formatter)
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
+
+
+logger.info("Program starts.")
 job_names = ['ping google.com', 'ls']
 
 for i in job_names:
