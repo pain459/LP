6d09ec05da1c2cfe00d9c57cce27ba01cf027bb2
Ravi <ravi.ameer@gmail.com>
2024-03-09 08:34:14 +0530

    Creating arrays.

---
+++ CPP/C7/p15.py
@@ -0,0 +1,6 @@
+# Create a single dimensional array using numpy
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])  # Create array
+
+print(arr)
---
+++ CPP/C7/p16.py
@@ -0,0 +1,5 @@
+# One more version of creating a array
+from numpy import *
+
+arr = array([10, 20, 30, 40, 50])
+print(arr)
