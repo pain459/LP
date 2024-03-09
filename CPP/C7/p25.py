f83a6ed784429740bc2a868096914bb1ae32aea5
Ravi <ravi.ameer@gmail.com>
2024-03-09 16:07:59 +0530

    Tried with alphabets.

--- CPP/C7/p25.py
+++ CPP/C7/p25.py
@@ -3,6 +3,8 @@ import numpy as np
 
 a = np.array([1, 2, 3, 0])
 b = np.array([0, 2, 3, 1])
+d = np.array(['a', 'b', 'c'])
+e = np.array(['a', 'd', 'f'])
 
 c = a == b
 print("Result of a = b:", c)
@@ -10,3 +12,8 @@ c = a > b
 print("Result of a > b:", c)
 c = a <= b
 print("Result of a <= b:", c)
+
+f = d == e
+print("Result of d = e:", f)
+f = d >= e
+print("Result of d > e:", f)
