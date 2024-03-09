32e36c63f7813434c7ea4e62f1ca48a90f8c5a3d
Ravi <ravi.ameer@gmail.com>
2024-03-09 08:39:48 +0530

    Creating more arrays in string and characters.

---
+++ CPP/C7/p17.py
@@ -0,0 +1,5 @@
+# Creating an array of characters
+from numpy import *
+
+arr = array(['a', 'b', 'c', 'd', 'e'])
+print(arr)
---
+++ CPP/C7/p18.py
@@ -0,0 +1,4 @@
+# Creating array with characters
+from numpy import *
+arr = array(['Delhi', 'Ahmedabad', 'Mumbai', 'Hyderabad'])
+print(arr)
