ddaab25a363064ca8f6d7bfc1f97bf8af71b7e8c
Ravi <ravi.ameer@gmail.com>
2024-03-10 19:11:14 +0530

    Corrected project structure and updated the gave an example as well.

---
+++ implementations/wheel_creation/factorial_project/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='factorial_package',
+    version='1.0.0',
+    packages=find_packages(),
+    author='pain',
+    author_email='pain@pain.com',
+    description='A simple package to calculate factorial of a number.',
+    url='https://github.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
