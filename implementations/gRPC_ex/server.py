c3653596f41a7a090043e21f5a9f8a4b3fe3c78d
Ravi <ravi.ameer@gmail.com>
2024-03-11 16:56:25 +0530

    gRCP implementation with example.

---
+++ implementations/gRPC_ex/server.py
@@ -0,0 +1,21 @@
+import grpc
+from concurrent import futures
+import example_pb2
+import example_pb2_grpc
+
+
+class ExampleServicer(example_pb2_grpc.ExampleServiceServicer):
+    def SayHello(self, request, context):
+        return example_pb2.HelloResponse(message=f"Hello, {request.name}!")
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    example_pb2_grpc.add_ExampleServiceServicer_to_server(ExampleServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
