7c0ecfe3db4c127269e5995ee47d1c71f3dfcbff
pain <ravi.ameer@gmail.com>
2024-03-14 07:01:22 +0530

    Testing unicode.

---
+++ CPP/C8/testing.py
@@ -0,0 +1,6 @@
+# Testing telugu unicode.
+# Ref https://en.wikipedia.org/wiki/Telugu_(Unicode_block)
+name = u'\u0C23'
+print(name)
+number = u'\u0C6A\u0C6B\u0C6F'  # Printing 459 from unicode.
+print(number)
