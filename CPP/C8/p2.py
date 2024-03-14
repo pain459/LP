f543e566c008ae34f71a25feb5817ef2a0daafce
pain <pain@pain>
2024-03-14 07:25:43 +0530

    Accessing string in different orders.

---
+++ CPP/C8/p2.py
@@ -0,0 +1,12 @@
+# Accessing elements of a string using for loop.
+str = "Core Python"
+
+# Access each letter using for loop
+for i in str:
+    print(i, end=' ')
+
+print()
+
+# Access in reversing order
+for i in str[::-1]:
+    print(i, end=' ')
