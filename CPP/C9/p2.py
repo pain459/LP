339afe477434f75ae5b7a42267e40b8e387675bd
pain <pain@pain>
2024-03-15 07:30:47 +0530

    Catching the returned results with functions.

---
+++ CPP/C9/p2.py
@@ -0,0 +1,9 @@
+# Function to add 2 numbers and return results
+
+def add_numbers(a, b):
+    """Function to add 2 numbers and return the result"""
+    return a + b
+
+
+x = add_numbers(10, 30)
+print(x)
