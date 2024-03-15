8efb0e8cb46bebc70ec2c5a671b59a7ce636beeb
pain <ravi.ameer@gmail.com>
2024-03-15 07:26:32 +0530

    Functions intro.

---
+++ CPP/C9/p1.py
@@ -0,0 +1,10 @@
+# A function to add 2 numbers
+
+def add_numbers(a, b):
+    """This function finds the sum of 2 numbers"""
+    c = a + b
+    print(f'Sum of {a} and {b} is {c}')
+
+
+# Calling a function
+add_numbers(10, 20)
