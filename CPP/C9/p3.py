826958bcf7382303c98ca479e68e5af1fca56a23
pain <ravi.ameer@gmail.com>
2024-03-15 07:36:36 +0530

    Find if number is even or odd.

---
+++ CPP/C9/p3.py
@@ -0,0 +1,10 @@
+# Test whether a number is even or not.
+def even_odd(num):
+    """To know num is even or odd"""
+    if num % 2 == 0:
+        print(f"{num} is even.")
+    else:
+        print(f"{num} is odd.")
+
+
+even_odd(13)
