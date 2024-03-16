a8c0fd2938de9cf948b75e460cc299f4791c26b3
pain459 <ravi.ameer@gmail.com>
2024-03-16 07:58:03 +0530

    Rewrite the commit history.

---
+++ staging_area/git_contribute_correct.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git filter-branch --env-filter '
+OLD_EMAIL="pain@pain"
+CORRECT_NAME="pain"
+CORRECT_EMAIL="ravi.ameer@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
