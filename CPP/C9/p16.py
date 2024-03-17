8420ae29ecead76d122d57bd8b391a27dba7cdb7
pain459 <ravi.ameer@gmail.com>
2024-03-17 17:02:04 +0530

    Positional arguments demo.

---
+++ CPP/C9/p16.py
@@ -0,0 +1,8 @@
+# Positional arguments demo
+def attach(s1, s2):
+    """To join s1 and s2 and display a full string"""
+    s3 = s1 + s2
+    print(f'Total String: {s3}')
+
+
+attach("Naruto", "Uzumaki")
