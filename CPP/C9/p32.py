09243ddab7cf8cf62a0d3717c70a29a67a93cb1d
Ravi <ravi.ameer@gmail.com>
2024-03-18 16:27:22 +0530

    Using lambda and filter to get even numbers from a list.

---
+++ CPP/C9/p32.py
@@ -0,0 +1,5 @@
+# a lambda function to return even numbers from the list.
+lst = [34, 55, 43, 23, 22, 56]
+# lst1 = list(filter(lambda x: (x % 2 == 0), lst))
+lst1 = list(filter(lambda x: (x % 2 == 0), lst))
+print(lst1)
