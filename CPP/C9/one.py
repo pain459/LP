586931b782e0b6e152a7504eb02fdb3f1f12c8a6
pain459 <ravi.ameer@gmail.com>
2024-03-20 21:39:47 +0530

    Demonstration of importing a program as a module using special variable __name__

---
+++ CPP/C9/one.py
@@ -0,0 +1,10 @@
+# Python program to display a message
+def display():
+    print("Hello from display function!")
+
+
+if __name__ == "__main__":
+    display()
+    print("This code is run as a program.")
+else:
+    print("This code is run as a module")
