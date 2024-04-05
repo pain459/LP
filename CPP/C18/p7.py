30e9e1f3d15f26d4f2cc24cd0914c6da2f0a606c
Ravi <ravi.ameer@gmail.com>
2024-04-05 11:03:13 +0530

    searching with space at the start and the end.

--- CPP/C18/p7.py
+++ CPP/C18/p7.py
@@ -4,3 +4,7 @@ import re
 str = 'deidara hidan itachi juzo kakuzu kisame konan ktes tkra orochimaru sasori tobi zetsu'
 result = re.findall(r'k[\w]*', str)
 print(result)
+
+# to add space before and after the word selection which eliminates false matches
+result = re.findall(r'\bk[\w]*\b', str)
+print(result)
