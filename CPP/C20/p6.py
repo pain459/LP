dfd8be2b7dbfd18be8d6a6bde5e01ba558563cc1
pain <ravi.ameer@gmail.com>
2024-04-07 23:08:35 +0530

    Find date and time using today()

---
+++ CPP/C20/p6.py
@@ -0,0 +1,11 @@
+# finding today date and time
+from datetime import *
+
+# today of datetime class ives date and time
+tdp = datetime.today()
+
+print(tdp)
+
+# today() of date gives the date only
+td = tdp.date()
+print(td)
