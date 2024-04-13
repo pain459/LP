38f4fc93d4c681268e2853e1fff73cea9416048c
pain <ravi.ameer@gmail.com>
2024-04-13 19:35:20 +0530

    Threadpool executor fed from the list.

---
+++ CPP/threading/threadpool_ex2.py
@@ -0,0 +1,26 @@
+import time
+import concurrent.futures
+
+
+def do_something(seconds):
+    print(f'Sleeping for {seconds} second(s)...')
+    time.sleep(seconds)
+    return f'Done sleeping for {seconds} second(s)...'
+
+
+def main():
+    t1 = time.perf_counter()
+    secs = [5, 4, 3, 2, 1]
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        threads = [executor.submit(do_something, sec) for sec in secs]
+
+        for thread in concurrent.futures.as_completed(threads):
+            print(thread.result())
+
+
+    t2 = time.perf_counter()
+    print(f'Finished in {round(t2 - t1, 2)} second(s)')
+    
+
+if __name__ == "__main__":
+    main()
