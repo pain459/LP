0ba77cc024e4a1177d5a0b5d56ff5ded2b40f469
pain <ravi.ameer@gmail.com>
2024-04-14 20:07:22 +0530

    multiprocessing basic implementation.

---
+++ CPP/multiprocessing/ex2.py
@@ -0,0 +1,28 @@
+# multiprocessing introduced.
+import multiprocessing.process
+import time
+import multiprocessing
+
+start_time =  time.perf_counter()
+
+def do_something():
+    print('Sleep for 1 second...')
+    time.sleep(1)
+    print('Done sleeping for 1 second...')
+
+# Create processes
+p1 = multiprocessing.Process(target=do_something)
+p2 = multiprocessing.Process(target=do_something)
+
+# Start the processes
+p1.start()
+p2.start()
+
+# wait for compeltion.
+p1.join()
+p2.join()
+
+end_time = time.perf_counter()
+
+
+print(f'Finished in {round(end_time - start_time, 2)} second(s)...')
