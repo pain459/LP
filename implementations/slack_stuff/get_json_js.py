20ad36404e8594d8a6f71b5d699e8f70f477a165
pain <ravi.ameer@gmail.com>
2024-04-19 08:37:03 +0530

    fetching data and parsing.

--- implementations/slack_stuff/get_json_js.py
+++ implementations/slack_stuff/get_json_js.py
@@ -5,6 +5,11 @@ response = requests.get(url)
 
 if response.status_code == 200:
     json_data = response.json()
-    print(json_data)
+    # print(json_data)
+    print('data fetched.')
 else:
     print(f"Failed to fetch JSON data. Status code: {response.status_code}")    
+
+
+title = json_data['title']
+print(title)
