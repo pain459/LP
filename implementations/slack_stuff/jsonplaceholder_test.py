4d05276d51af10cff79946c1f38be291e71dd8ce
pain <ravi.ameer@gmail.com>
2024-04-21 07:57:34 +0530

    json to dataframe

--- implementations/slack_stuff/jsonplaceholder_test.py
+++ implementations/slack_stuff/jsonplaceholder_test.py
@@ -1,13 +1,16 @@
 import requests
+import pandas as pd
 
 url = 'https://jsonplaceholder.typicode.com/albums'
-r = requests.get(url)
+response = requests.get(url)
 
-# print(r.status_code)
+if response.status_code == 200:
+    # parse json data
+    json_data = response.json()
 
-if r.status_code == 200:
-    print('response received.')
+    # Convert JSON data to dataframe
+    df = pd.DataFrame.from_dict(json_data)
+    print(df.head())
+
 else:
-    print('error')
-
-# print(r.json())
+    print(f'Failed to reetrive response {response.status_code}')
