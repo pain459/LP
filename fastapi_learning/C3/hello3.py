5d0c593ecee88f058a4f40d6d61e01260bcb23fb
pain <ravi.ameer@gmail.com>
2024-04-21 22:43:15 +0530

    using query parameters.

---
+++ fastapi_learning/C3/hello3.py
@@ -0,0 +1,13 @@
+# Using query parameters
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/hi")
+def greet(who):
+    return f'Hello {who}?'
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("hello3:app", reload=True)
