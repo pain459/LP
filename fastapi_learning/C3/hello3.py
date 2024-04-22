ae7f811e4e566ff69c5fbbb0cf5413db7946f3ea
Ravi <ravi.ameer@gmail.com>
2024-04-22 23:35:00 +0530

    snaped api.

--- fastapi_learning/C3/hello3.py
+++ fastapi_learning/C3/hello3.py
@@ -3,11 +3,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/hi")
 def greet(who):
     return f'Hello {who}?'
 
 
+@app.get("/snape")
+def snape(name):
+    return f'Page no. 269.'
+
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("hello3:app", reload=True)
