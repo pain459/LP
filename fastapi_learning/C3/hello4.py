956c560acc7da80b151e84a0e06b7369f726671c
pain <ravi.ameer@gmail.com>
2024-04-23 20:48:45 +0530

    adding post method and passing json.

---
+++ fastapi_learning/C3/hello4.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+@app.post("/hi")
+def greet(who:str = Body(embed=True)):
+    return f'Hello? {who}'
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("hello4:app", reload=True)
