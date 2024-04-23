5affc6b48ad9ea248aed096758d96a61d6ab9158
pain <ravi.ameer@gmail.com>
2024-04-23 23:24:59 +0530

    plan for password generator script.

---
+++ random_scripts/password_generator.py
@@ -0,0 +1,16 @@
+# Script to generate passwords of required lengths.
+
+# imports
+
+
+# function to generate 3 random characters from upper case, special symbol and number
+
+
+# generate a password with length -3 .
+
+
+# combine the password with random letters from func1 and shuffle
+
+
+# main function
+# display the password
