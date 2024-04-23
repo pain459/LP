dd7fc1d804bb10a528dc1745e7ba941db68331e0
pain <ravi.ameer@gmail.com>
2024-04-23 23:28:01 +0530

    skeleton.

--- random_scripts/password_generator.py
+++ random_scripts/password_generator.py
@@ -1,16 +1,30 @@
 # Script to generate passwords of required lengths.
 
 # imports
+import random
+import string
 
 
 # function to generate 3 random characters from upper case, special symbol and number
-
+def generate_special_chars():
+    return -1
 
 # generate a password with length -3 .
+def generate_password_of_length():
+    return -1
 
 
 # combine the password with random letters from func1 and shuffle
+def generate_final_password():
+    return -1
 
 
 # main function
+
+def main():
+    print("Program completed!")
 # display the password
+
+
+if __name__ == "__main__":
+    main()
