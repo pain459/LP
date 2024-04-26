c2717d9060c501cd90280419bda6332636b2471a
pain <ravi.ameer@gmail.com>
2024-04-26 09:25:51 +0530

    Filtered out even numbers only.

---
+++ python_tricks/filter_out_even.py
@@ -0,0 +1,8 @@
+# original list
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# filter out even numbers using list comprehension
+filtered = [i for i in numbers if i % 2 == 0]
+
+# filtered even numbers only
+print(filtered)
