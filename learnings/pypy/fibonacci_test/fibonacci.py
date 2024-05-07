3f9531478674435dd04a500cf9198d747401926f
pain <ravi.ameer@gmail.com>
2024-05-07 08:45:52 +0530

    fibonacci series program

---
+++ learnings/pypy/fibonacci_test/fibonacci.py
@@ -0,0 +1,24 @@
+import time
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def measure_time(n):
+    start_time = time.time()
+    result = fibonacci(n)
+    end_time = time.time()
+    print(f'Fibonacci of {n} is {result}')
+    print(f'Time taken is {end_time - start_time} seconds.')
+
+
+def main():
+    n = 2999
+    measure_time(n)
+
+
+if __name__ == "__main__":
+    main()
