84bec308a629f3d949af53bcacd1859d673370a5
pain <ravi.ameer@gmail.com>
2024-05-07 08:23:31 +0530

    jar file path update

--- implementations/jar_from_python/launch_jar.py
+++ implementations/jar_from_python/launch_jar.py
@@ -5,14 +5,14 @@ def run_jar(jar_file):
     '''Launch jar from python'''
     try:
         # launch the script
-        subprocess.run(['java', jar_file], check=True)
+        subprocess.run(['java', '-jar', jar_file], check=True)
     except subprocess.CalledProcessError as e:
         print(f'Java application failed {e}')
     except Exception as e:
         print(f'An error occurred {e}')
 
 def main():
-    run_jar('helloWorld.java')
+    run_jar('helloWorld.jar')
 
 if __name__ == "__main__":
     main()
