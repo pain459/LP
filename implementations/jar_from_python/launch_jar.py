4d3024b7822f946326c0854d7aa65779120d3fd5
pain <ravi.ameer@gmail.com>
2024-05-07 08:22:21 +0530

    execute jar from python.

---
+++ implementations/jar_from_python/launch_jar.py
@@ -0,0 +1,18 @@
+import subprocess
+
+
+def run_jar(jar_file):
+    '''Launch jar from python'''
+    try:
+        # launch the script
+        subprocess.run(['java', jar_file], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f'Java application failed {e}')
+    except Exception as e:
+        print(f'An error occurred {e}')
+
+def main():
+    run_jar('helloWorld.java')
+
+if __name__ == "__main__":
+    main()
