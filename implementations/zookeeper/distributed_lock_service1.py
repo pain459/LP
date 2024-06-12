75806dcf0520500cdd7ddaf76a531bb86238c2c0
Pain <ravi.ameer@gmail.com>
2024-06-12 10:02:00 +0530

    distributed lock example.

---
+++ implementations/zookeeper/distributed_lock_service1.py
@@ -0,0 +1,24 @@
+from kazoo.client import KazooClient
+import time
+
+def distributed_task():
+    zk = KazooClient(hosts='127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183')
+    zk.start()
+
+    lock = zk.Lock("/distributed_lock", "service-1")
+
+    while True:
+        print("Service 1 trying to acquire lock")
+        with lock:  # This will block until the lock is acquired
+            print("Service 1 acquired lock")
+            # Simulate some work with the shared resource
+            time.sleep(5)
+            print("Service 1 releasing lock")
+        
+        # Sleep before trying to acquire the lock again
+        time.sleep(3)
+
+    zk.stop()
+
+if __name__ == "__main__":
+    distributed_task()
