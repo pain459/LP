c563b5973979d7fc2dbc38440050c7d16254adef
Pain <ravi.ameer@gmail.com>
2024-06-22 21:34:05 +0530

    refinement to the pages.

--- system_design/url_shortner/app.py
+++ system_design/url_shortner/app.py
@@ -14,23 +14,49 @@ def generate_short_code(length=6):
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    urls = db.all()
+    return render_template('index.html', urls=urls)
 
 @app.route('/shorten', methods=['POST'])
 def shorten_url():
     original_url = request.form['url']
-    short_code = generate_short_code()
-    db.insert({'original_url': original_url, 'short_code': short_code})
+    custom_shortname = request.form['shortname']
+    Url = Query()
+
+    if custom_shortname:
+        short_code = custom_shortname
+        if db.search(Url.short_code == short_code):
+            return render_template('index.html', error="Custom short name already exists!", urls=db.all())
+    else:
+        result = db.search(Url.original_url == original_url)
+        if result:
+            short_code = result[0]['short_code']
+        else:
+            short_code = generate_short_code()
+            while db.search(Url.short_code == short_code):
+                short_code = generate_short_code()
+
+    if not db.search(Url.original_url == original_url):
+        db.insert({'original_url': original_url, 'short_code': short_code, 'hits': 0})
+
     short_url = request.host_url + short_code
-    return render_template('index.html', short_url=short_url)
+    return render_template('index.html', short_url=short_url, urls=db.all())
 
 @app.route('/<short_code>', methods=['GET'])
 def redirect_to_url(short_code):
     Url = Query()
     result = db.search(Url.short_code == short_code)
     if result:
+        db.update({'hits': result[0]['hits'] + 1}, Url.short_code == short_code)
         return redirect(result[0]['original_url'])
     return 'URL not found', 404
+
+@app.route('/delete', methods=['POST'])
+def delete_url():
+    short_code = request.form['short_code']
+    Url = Query()
+    db.remove(Url.short_code == short_code)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
