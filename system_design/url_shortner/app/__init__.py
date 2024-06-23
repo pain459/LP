ec12e3a5a22909414e20396d564a76fa2bf8793d
Pain <ravi.ameer@gmail.com>
2024-06-23 12:43:20 +0530

    Basic UI and working CRUD operations.

--- system_design/url_shortner/app/__init__.py
+++ system_design/url_shortner/app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=os.path.abspath('templates'), static_folder=os.path.abspath('static'))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urls.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
--- system_design/url_shortner/app/views.py
+++ system_design/url_shortner/app/views.py
@@ -1,73 +1,47 @@
-from flask import request, jsonify
+from flask import request, jsonify, render_template, redirect, url_for
 from app import app, db
 from app.models import URL
 from app.utils import generate_hash
 
-@app.route('/shorten', methods=['POST'])
-def shorten_url():
-    original_url = request.json.get('original_url')
-    
-    existing_url = URL.query.filter_by(original_url=original_url).first()
-    if existing_url:
-        return jsonify({
-            'message': 'URL is already shortened',
-            'shortened_url': existing_url.shortened_url
-        }), 200
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        original_url = request.form.get('original_url')
+        existing_url = URL.query.filter_by(original_url=original_url).first()
+        if existing_url:
+            return redirect(url_for('index'))
 
-    url_hash = generate_hash(original_url)
-    shortened_url = f"http://short.url/{url_hash}"
+        url_hash = generate_hash(original_url)
+        shortened_url = f"http://short.url/{url_hash}"
 
-    new_url = URL(original_url=original_url, shortened_url=shortened_url, url_hash=url_hash)
-    db.session.add(new_url)
-    db.session.commit()
+        new_url = URL(original_url=original_url, shortened_url=shortened_url, url_hash=url_hash)
+        db.session.add(new_url)
+        db.session.commit()
 
-    return jsonify({
-        'original_url': original_url,
-        'shortened_url': shortened_url
-    }), 201
+        return redirect(url_for('index'))
 
-@app.route('/<url_hash>', methods=['GET'])
-def retrieve_url(url_hash):
-    url = URL.query.filter_by(url_hash=url_hash).first()
+    urls = URL.query.all()
+    return render_template('index.html', urls=urls)
+
+@app.route('/delete/<int:url_id>', methods=['POST'])
+def delete_url(url_id):
+    url = URL.query.get(url_id)
     if url:
-        return jsonify({'original_url': url.original_url}), 200
-    return jsonify({'message': 'URL not found'}), 404
+        db.session.delete(url)
+        db.session.commit()
+    return redirect(url_for('index'))
 
-@app.route('/update', methods=['PUT'])
-def update_url():
-    original_url = request.json.get('original_url')
-    new_original_url = request.json.get('new_original_url')
-    
-    url_hash = generate_hash(original_url)
-    url = URL.query.filter_by(url_hash=url_hash).first()
-    if not url:
-        return jsonify({'message': 'URL not found'}), 404
+@app.route('/update/<int:url_id>', methods=['POST'])
+def update_url(url_id):
+    new_original_url = request.form.get('new_original_url')
+    url = URL.query.get(url_id)
+    if url:
+        new_hash = generate_hash(new_original_url)
+        new_shortened_url = f"http://short.url/{new_hash}"
 
-    new_hash = generate_hash(new_original_url)
-    new_shortened_url = f"http://short.url/{new_hash}"
+        url.original_url = new_original_url
+        url.url_hash = new_hash
+        url.shortened_url = new_shortened_url
 
-    url.original_url = new_original_url
-    url.url_hash = new_hash
-    url.shortened_url = new_shortened_url
-
-    db.session.commit()
-
-    return jsonify({
-        'message': 'URL updated successfully',
-        'original_url': url.original_url,
-        'shortened_url': url.shortened_url
-    }), 200
-
-@app.route('/delete', methods=['DELETE'])
-def delete_url():
-    original_url = request.json.get('original_url')
-    url_hash = generate_hash(original_url)
-    
-    url = URL.query.filter_by(url_hash=url_hash).first()
-    if not url:
-        return jsonify({'message': 'URL not found'}), 404
-
-    db.session.delete(url)
-    db.session.commit()
-
-    return jsonify({'message': 'URL deleted successfully'}), 200
+        db.session.commit()
+    return redirect(url_for('index'))
