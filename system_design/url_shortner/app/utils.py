5ebff5bd4e12169353fcdb2ec60b629e1fe018a1
Pain <ravi.ameer@gmail.com>
2024-06-23 12:49:20 +0530

    URL redirection is working as expected.

--- system_design/url_shortner/app/utils.py
+++ system_design/url_shortner/app/utils.py
@@ -2,3 +2,6 @@ import hashlib
 
 def generate_hash(url):
     return hashlib.md5(url.encode()).hexdigest()[:10]
+
+def generate_short_url(original_url):
+    return hashlib.sha256(original_url.encode()).hexdigest()[:10]
--- system_design/url_shortner/app/views.py
+++ system_design/url_shortner/app/views.py
@@ -1,47 +1,48 @@
-from flask import request, jsonify, render_template, redirect, url_for
+from flask import render_template, request, redirect, url_for, jsonify
 from app import app, db
 from app.models import URL
-from app.utils import generate_hash
+from app.utils import generate_short_url
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        original_url = request.form.get('original_url')
-        existing_url = URL.query.filter_by(original_url=original_url).first()
-        if existing_url:
-            return redirect(url_for('index'))
-
-        url_hash = generate_hash(original_url)
-        shortened_url = f"http://short.url/{url_hash}"
-
-        new_url = URL(original_url=original_url, shortened_url=shortened_url, url_hash=url_hash)
-        db.session.add(new_url)
-        db.session.commit()
-
-        return redirect(url_for('index'))
-
     urls = URL.query.all()
     return render_template('index.html', urls=urls)
 
+@app.route('/', methods=['POST'])
+def shorten_url():
+    original_url = request.form['original_url']
+    existing_url = URL.query.filter_by(original_url=original_url).first()
+    if existing_url:
+        return jsonify({
+            'message': 'URL is already shortened',
+            'shortened_url': existing_url.shortened_url
+        }), 200
+
+    url_hash = generate_short_url(original_url)
+    shortened_url = url_for('redirect_to_original', url_hash=url_hash, _external=True)
+    new_url = URL(original_url=original_url, shortened_url=shortened_url, url_hash=url_hash)
+    db.session.add(new_url)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/<url_hash>')
+def redirect_to_original(url_hash):
+    url = URL.query.filter_by(url_hash=url_hash).first_or_404()
+    return redirect(url.original_url)
+
 @app.route('/delete/<int:url_id>', methods=['POST'])
 def delete_url(url_id):
-    url = URL.query.get(url_id)
-    if url:
-        db.session.delete(url)
-        db.session.commit()
+    url = URL.query.get_or_404(url_id)
+    db.session.delete(url)
+    db.session.commit()
     return redirect(url_for('index'))
 
 @app.route('/update/<int:url_id>', methods=['POST'])
 def update_url(url_id):
-    new_original_url = request.form.get('new_original_url')
-    url = URL.query.get(url_id)
-    if url:
-        new_hash = generate_hash(new_original_url)
-        new_shortened_url = f"http://short.url/{new_hash}"
-
-        url.original_url = new_original_url
-        url.url_hash = new_hash
-        url.shortened_url = new_shortened_url
-
-        db.session.commit()
+    new_original_url = request.form['new_original_url']
+    url = URL.query.get_or_404(url_id)
+    url.original_url = new_original_url
+    url.url_hash = generate_short_url(new_original_url)
+    url.shortened_url = url_for('redirect_to_original', url_hash=url.url_hash, _external=True)
+    db.session.commit()
     return redirect(url_for('index'))
