1b0ea0cce8ea1f91eba22586d7b83aceb73ca6e3
Pain <ravi.ameer@gmail.com>
2024-07-11 07:51:11 +0530

    data load using insert scripts.

--- projects/cricket_ratings_dashboard/app/models.py
+++ projects/cricket_ratings_dashboard/app/models.py
@@ -2,7 +2,7 @@ from . import db
 
 class CountryRanking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    unique_id = db.Column(db.String(50), unique=True, nullable=False)
+    unique_id = db.Column(db.String(10), unique=True, nullable=False)
     country = db.Column(db.String(100), nullable=False)
     points = db.Column(db.Integer, nullable=False)
 
---
+++ projects/cricket_ratings_dashboard/insert_data.py
@@ -0,0 +1,44 @@
+# insert_data.py
+import random
+from app import create_app, db
+from app.models import CountryRanking
+
+app = create_app()
+app.app_context().push()
+
+# List of country codes (example)
+country_codes = {
+    'Afghanistan': 'AF',
+    'Australia': 'AU',
+    'Bangladesh': 'BD',
+    'England': 'GB',
+    'India': 'IN',
+    'Ireland': 'IE',
+    'New Zealand': 'NZ',
+    'Pakistan': 'PK',
+    'South Africa': 'ZA',
+    'Sri Lanka': 'LK',
+    'West Indies': 'WI',
+    'Zimbabwe': 'ZW',
+    'Nepal': 'NP',
+    'Netherlands': 'NL',
+    'Scotland': 'SC',
+    'UAE': 'AE',
+    'USA': 'US',
+    'Canada': 'CA'
+}
+
+def generate_points():
+    return random.randint(0, 2500)
+
+def insert_countries():
+    for country, code in country_codes.items():
+        unique_id = f"{code}_0001"
+        points = generate_points()
+        ranking = CountryRanking(unique_id=unique_id, country=country, points=points)
+        db.session.add(ranking)
+    db.session.commit()
+
+if __name__ == "__main__":
+    insert_countries()
+    print("Data inserted successfully!")
