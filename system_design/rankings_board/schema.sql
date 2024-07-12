7c5d0fc7e31dc8cde5ed7ced1291e2ba6bc41955
Pain <ravi.ameer@gmail.com>
2024-07-12 21:18:10 +0530

    Schema update.

--- system_design/rankings_board/schema.sql
+++ system_design/rankings_board/schema.sql
@@ -1,14 +1,14 @@
--- Create the schema
-CREATE SCHEMA rankings_board;
+-- Create the schema (if it does not already exist)
+CREATE SCHEMA IF NOT EXISTS rankings_board;
 
--- Create the country_codes table
-CREATE TABLE rankings_board.country_codes (
+-- Create the country_codes table (if it does not already exist)
+CREATE TABLE IF NOT EXISTS rankings_board.country_codes (
     country_name VARCHAR(255) NOT NULL,
     country_code CHAR(2) PRIMARY KEY
 );
 
--- Create the player_names table
-CREATE TABLE rankings_board.player_names (
+-- Create the player_names table (if it does not already exist)
+CREATE TABLE IF NOT EXISTS rankings_board.player_names (
     unique_id CHAR(64) PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     middle_name VARCHAR(255),
@@ -19,7 +19,19 @@ CREATE TABLE rankings_board.player_names (
     FOREIGN KEY (country_code) REFERENCES rankings_board.country_codes(country_code)
 );
 
+-- Create the player_stats table
+CREATE TABLE IF NOT EXISTS rankings_board.player_stats (
+    unique_id CHAR(64) PRIMARY KEY,
+    matches INTEGER NOT NULL,
+    goals INTEGER NOT NULL,
+    assists INTEGER NOT NULL,
+    fouls INTEGER NOT NULL,
+    injuries INTEGER NOT NULL,
+    FOREIGN KEY (unique_id) REFERENCES rankings_board.player_names(unique_id)
+);
+
 -- Verify the creation
 \dt rankings_board.*
 \d rankings_board.country_codes
 \d rankings_board.player_names
+\d rankings_board.player_stats
