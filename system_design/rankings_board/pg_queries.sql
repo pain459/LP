df7cb4bb557cd3779b00b4788af8621110c73658
Pain <ravi.ameer@gmail.com>
2024-07-13 07:44:35 +0530

    changes:
    1. Insert new data cleverly.
    2. notes with refresh material view command.
    3. sql queries dump added.

---
+++ system_design/rankings_board/pg_queries.sql
@@ -0,0 +1,6 @@
+select count(*) from rankings_board.country_codes;
+
+select count(*) from rankings_board.player_names;
+select count(*) from rankings_board.player_stats;
+select count(*) from rankings_board.player_status_materialized;
+
