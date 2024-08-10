712f82ca106f4ade5170325c22c6432d59fc1cea
pain <ravi.ameer@gmail.com>
2024-08-10 08:00:37 +0530

    app setup to support CRUD. migration scripts added.

--- projects/flask_task_manager/web/app.py
+++ projects/flask_task_manager/web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Define the User model
+# Models
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -21,7 +21,6 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
-# Define the Task model
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
@@ -33,6 +32,59 @@ class Task(db.Model):
 
     def __repr__(self):
         return f'<Task {self.title}>'
+
+# Routes
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks = Task.query.all()
+    return jsonify([{
+        'id': task.id,
+        'title': task.title,
+        'description': task.description,
+        'is_done': task.is_done
+    } for task in tasks])
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    return jsonify({
+        'id': task.id,
+        'title': task.title,
+        'description': task.description,
+        'is_done': task.is_done
+    })
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    new_task = Task(
+        title=data['title'],
+        description=data.get('description', ''),
+        is_done=data.get('is_done', False),
+        user_id=data['user_id']
+    )
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({'message': 'Task created successfully!'}), 201
+
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    data = request.get_json()
+
+    task.title = data['title']
+    task.description = data.get('description', task.description)
+    task.is_done = data.get('is_done', task.is_done)
+
+    db.session.commit()
+    return jsonify({'message': 'Task updated successfully!'})
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({'message': 'Task deleted successfully!'})
 
 @app.route('/')
 def hello():
