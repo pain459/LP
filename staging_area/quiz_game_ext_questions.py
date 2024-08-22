600030c1aff21c172f2fd0d662b99b52c887bb5e
pain <ravi.ameer@gmail.com>
2024-08-22 08:45:59 +0200

    reading questions

--- staging_area/quiz_game_ext_questions.py
+++ staging_area/quiz_game_ext_questions.py
@@ -4,5 +4,10 @@ import json
 with open('DB.json', 'r') as file:
     data = json.load(file)
 
-# Access data
-print(data)
+# # Access data
+# print(data[1])
+# # Access question
+# print(data[1]["question"])
+# print(data[1]["answers"])
+# print(data[1]["answers"][0:3])
+
