963bcc47904bc8db107c46de19870c65a2643ae8
pain <ravi.ameer@gmail.com>
2024-08-23 22:44:26 +0200

    structure setup

--- staging_area/quiz_game_ext_questions.py
+++ staging_area/quiz_game_ext_questions.py
@@ -15,7 +15,7 @@ with open('DB.json', 'r') as file:
 
 total_questions = len(data)
 score = 0
-questions_per_game = 3
+questions_per_game = 1
 selected_question = []
 
 def choose_random_question():
@@ -24,10 +24,13 @@ def choose_random_question():
 for i in range(questions_per_game):
     extract_question = choose_random_question()
     selected_question.append(extract_question)
+    print(data[extract_question])
     print(f'Your question is {extract_question}')
     print(f'{data[extract_question]["question"]}')
     print("Your options: \n")
     for options in data[extract_question]["answers"]:
         print(f"{options['choice']}: {options['text']}")
+    answer = input("Your answer? ").strip().upper()
+    print(data[extract_question]["question"]["answers"][0]['correct'])
 
 print(selected_question)
