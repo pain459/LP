65a73dd592fa58a2212c835c60675ba32674f858
pain <ravi.ameer@gmail.com>
2024-08-24 08:38:21 +0200

    func arrangement

--- staging_area/quiz_game_ext_questions.py
+++ staging_area/quiz_game_ext_questions.py
@@ -6,27 +6,31 @@ with open('DB.json', 'r') as file:
     data = json.load(file)
 
 total_questions = len(data)
-score = 0
-questions_per_game = 3
-selected_question = []
 
 def choose_random_question():
     return random.randint(0, total_questions)
 
-for i in range(questions_per_game):
-    extract_question = choose_random_question()
-    selected_question.append(extract_question)
-    print(f'Your question is {extract_question}')
-    print(f'{data[extract_question]["question"]}')
-    print("Your options: \n")
-    for options in data[extract_question]["answers"]:
-        print(f"{options['choice']}: {options['text']}")
-    answer = input("Your answer? ").strip().upper()
-    for i in data[extract_question]["answers"]:
-        if (i['choice'] == answer) & (i['correct']):
-            print('Correct!')
-            score += 1
-        else:
-            pass
+def main():
+    score = 0
+    questions_per_game = 3
+    selected_question = []
+    for i in range(questions_per_game):
+        extract_question = choose_random_question()
+        selected_question.append(extract_question)
+        print(f'Your question is {extract_question}')
+        print(f'{data[extract_question]["question"]}')
+        print("Your options: \n")
+        for options in data[extract_question]["answers"]:
+            print(f"{options['choice']}: {options['text']}")
+        answer = input("Your answer? ").strip().upper()
+        for i in data[extract_question]["answers"]:
+            if (i['choice'] == answer) & (i['correct']):
+                print('Correct!')
+                score += 1
+            else:
+                pass
 
-print(f"You scored {score} out of {questions_per_game}")
+    print(f"You scored {score} out of {questions_per_game}")
+
+if __name__ == "__main__":
+    main()
