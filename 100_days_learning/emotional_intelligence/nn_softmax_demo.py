e24c6ca3c9a91596263102c5ed7f5de47d029374
Pain <ravi.ameer@gmail.com>
2024-12-28 11:46:28 +0530

    plan to run demo.

---
+++ 100_days_learning/emotional_intelligence/nn_softmax_demo.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Step 1: Define the neural network
+
+
+# Step 2: Instantiate the network
+
+
+# Step 3: Enable input Tensor
+
+
+# Step 4: Forward pass through network
+
+
+# Step 5: Apply Softmax to Convert Logits to probabilities
+
+
+# Step 6: Predicted Class
