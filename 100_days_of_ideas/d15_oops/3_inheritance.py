7500e31cf05fdb230e642398e9316ca15e880c1f
Pain <ravi.ameer@gmail.com>
2024-12-29 08:48:32 +0530

    inheritance example.

---
+++ 100_days_of_ideas/d15_oops/3_inheritance.py
@@ -0,0 +1,14 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def sound(self):
+        return "Some generic sound."
+    
+# Child class
+class Cat(Animal):
+    def sound(self):
+        return f"{self.name} says Meow!"
+    
+my_cat = Cat("Orion")
+print(my_cat.sound())
