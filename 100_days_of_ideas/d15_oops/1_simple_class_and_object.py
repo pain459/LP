022d059be3a0a5aa1d34e9d76002b7c3f5e7feb8
Pain <ravi.ameer@gmail.com>
2024-12-29 08:16:11 +0530

    testing positional arguments

--- 100_days_of_ideas/d15_oops/1_simple_class_and_object.py
+++ 100_days_of_ideas/d15_oops/1_simple_class_and_object.py
@@ -12,9 +12,15 @@ class Dog:
 
 # create an object (instance of the class)
 my_dog = Dog("Buddy", "Doberman")
+my_dog2 = Dog("Labrador", "Jackie")
+
 
 
 # Access attributes and methods
 print(my_dog.name)
 print(my_dog.bark())
 print(my_dog.breed)
+
+# Access in reverse. Testing positional arguments
+print(my_dog2.name)  # Should print Labrador
+print(my_dog2.breed)  # Should print Jackie
